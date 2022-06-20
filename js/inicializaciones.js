672f4fcf9bc617f8f0812a733f839b5a00c12ce3
urr13l <102545461+urr13l@users.noreply.github.com>
2022-06-20 18:19:39 -0500

    primerCambio

--- js/inicializaciones.js
+++ js/inicializaciones.js
@@ -3,17 +3,38 @@ document.addEventListener('DOMContentLoaded', function() {
   var modalInicio = document.querySelectorAll('#modalInicio');
   var dropdownPerfil = document.querySelectorAll('#tiggerPerfil');
   var sidevarInicio = document.querySelectorAll('.sidenav');
+  var select = document.querySelectorAll('select');
+  var datePicker = document.querySelectorAll('.datepicker');
+
 
   var instances = M.Modal.init(elems);
   var instanciaInicio = M.Modal.init(modalInicio);
+
   var instanciaDropPerfil = M.Dropdown.init(dropdownPerfil, {
     coverTrigger: false,
     hover: true,
     inDuration: 160,
     outDuration: 500
   });
+
   var instanciaSideInicio = M.Sidenav.init(sidevarInicio, {
     inDuration: 300, 
     outDuration: 250
   });
+
+  var instanciaSelect = M.FormSelect.init(select);
+  
+  var instanciaDate = M.Datepicker.init(datePicker, {
+    autoClose: true,
+    format: 'dd mmmm yyyy',
+    i18n: {
+      months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+      monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+      weekdays: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+      weekdaysShort: ['Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa', 'Do'],
+      weekdaysAbbrev: ['L', 'M', 'MM', 'J', 'V', 'S', 'D']
+    }
+  });
+    
 });
+
--- viajes.php
+++ viajes.php
@@ -1,26 +1,22 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <!--Import Google Icon Font-->
-    <link
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet"
-    />
-    
-    <!--Import materialize.css-->
-    <link
-      type="text/css"
-      rel="stylesheet"
-      href="css/materialize.min.css"
-      media="screen,projection"
-    />
 
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Viajes</title>
-  </head>
-  <body>
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+
+  <link type="text/css" rel="stylesheet" href="css/fondos.css" media="screen,projection" />
+
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Viajes</title>
+</head>
+
+<body>
   <nav class="light-blue darken-4">
     <div class="nav-wrapper container">
       <a href="" class="brand-logo">
@@ -49,6 +45,96 @@
     </div>
   </nav>
 
+  <article>
+    <section>
+      <br>
+      <div id="fondo">
+        <div class="container">
+          <br>
+          <h6>¿A donde te vamos a llevar?</h6>
+          <form method="POST">
+            <div class="row">
+              <div class="input-field col s4 m2 l2">
+                <select>
+                  <option value="1">Ida</option>
+                  <option value="2">Ida y vuelta</option>
+                </select>
+              </div>
+
+              <div class="input-field col s2 m1 l1">
+                <input id="selectAdultos" type="number" class="validate">
+                <label for="selectAdultos">Adulto(s)</label>
+              </div>
+
+              <div class="input-field col s2 m1 l1">
+                <input id="selectMenores" type="number" class="validate">
+                <label for="selectMenores">Niño(s)</label>
+              </div>
+
+              <div class="input-field col s2 m1 l1">
+                <input id="selectInapam" type="number" class="validate">
+                <label for="selectInapam">INAPAM</label>
+              </div>
+            </div>
+
+            <div class="row">
+
+              <div class="input-field col s3">
+                <select>
+                  <option value="0" disabled selected>Desde donde viaja</option>
+                  <option value="1">Puerto Escondido</option>
+                  <option value="2">Oaxaca capital</option>
+                  <option value="3">Huatulco</option>
+                  <option value="4">Pinotepa</option>
+                  <option value="5">San gabriel</option>
+                </select>
+                <label>Origen</label>
+              </div>
+
+              <div class="input-field col s3">
+                <select name="selectViaje">
+                  <option value="0" disabled selected>Hacia donde viaja</option>
+                  <option value="Puerto Escondido">Puerto Escondido</option>
+                  <option value="Oaxaca capital">Oaxaca capital</option>
+                  <option value="Huatulco">Huatulco</option>
+                  <option value="Pinotepa">Pinotepa</option>
+                  <option value="San gabriel">San gabriel</option>
+                </select>
+                <label>Destino</label>
+              </div>
+
+              <div class="input-field col s3">
+                <input type="text" class="datepicker" id="fechaViaje">
+                <label for="fechaViaje">Fecha de salida</label>
+              </div>
+
+              <div class="input-field col s3">
+                <button class="btn waves-effect waves-light light-blue darken-4" type="submit" name="buscarViaje">
+                  Buscar viaje
+                </button>
+              </div>
+
+            </div>
+          </form>
+          <br>
+        </div>
+      </div>
+    </section>
+  </article>
+
+
+  <?php
+    if(isset($_POST['buscarViaje'])) {
+      $destinoViaje = $_POST['selectViaje'];
+
+      if ($destinoViaje == "Puerto Escondido") {
+        echo "<script> window.location.href = \"disponibilidadViaje.php\"</script> </script>";
+      } else {
+        echo "<script> alert(' Sin destino '); </script>";
+      }
+    }
+  ?>
+
   <!--opciones de menú "hamburguesa responsive"-->
   <ul class="sidenav" id="menu-responsive">
     <li><a href="">Viajes</a></li>
@@ -57,5 +143,11 @@
     <li><a href="index.php">Salir</a></li>
   </ul>
 
-  </body>
+
+  <script type="text/javascript" src="js/inicializaciones.js"></script>
+  <script type="text/javascript" src="js/materialize.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+
+</body>
+
 </html>
